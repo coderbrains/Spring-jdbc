c4a741d8fa97d045eba8158d066b959e16940af0
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-06-29 21:57:35 +0530

    all the hibernate queries have been failed

--- springorm/src/main/java/com/spring/orm/App.java
+++ springorm/src/main/java/com/spring/orm/App.java
@@ -1,5 +1,11 @@
 package com.spring.orm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.orm.dao.Studentdao;
+import com.spring.orm.entites.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+    	System.out.println("APP started....");
+    	
+    	ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+    	
+    	Studentdao studentdao = context.getBean("studentDao",Studentdao.class);
+    	
+    	Student student = new Student();
+    	
+    	student.setCity("siwan");
+    	student.setId(12);
+    	student.setName("Awanish");
+    	
+    	int i = studentdao.insert(student);
+    	System.out.println("successfully added..." + i );
+    
     }
 }
--- springorm/src/main/java/com/spring/orm/dao/Studentdao.java
+++ springorm/src/main/java/com/spring/orm/dao/Studentdao.java
@@ -1,15 +1,40 @@
 package com.spring.orm.dao;
 
+import java.io.Serializable;
 import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import com.spring.orm.entites.Student;
 import com.spring.orm.interfaces.StudentIn;
 
+
+@Transactional
 public class Studentdao implements StudentIn{
+	
+	HibernateTemplate hibernateTemplate;
+
+	/**
+	 * @return the hibernateTemplate
+	 */
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	/**
+	 * @param hibernateTemplate the hibernateTemplate to set
+	 */
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
 
 	public int insert(Student student) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int save = (Integer)hibernateTemplate.save(student);
+		
+		return save;
 	}
 
 	public int update(Student student) {
